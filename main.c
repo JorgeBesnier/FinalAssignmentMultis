ceb1574a5633c6a30f6464bbc53fcc0c904f2796
Bernardo G <bernardoc.gutierrezc@gmail.com>
2021-11-08 21:07:40 -0600

    division de ttrabajo

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+// Leer datos
+
+// Realizar calculo
+
+// Escribir resultados
+
+
+int main() {
+
+    // Llamada a leer datos
+    // Llamada a realizar calculo
+    // Llamada a escribir resultados
+
+    return 0;
+}
