fe5071f6fabdb29f427ce63d71a4b38887194620
Bernardo G <bernardoc.gutierrezc@gmail.com>
2021-11-09 21:24:23 -0600

    Added matrix_multiplication function

--- main.c
+++ main.c
@@ -25,23 +25,55 @@ void print_matrix(MatDims *dims, double mat[dims->y][dims->x]) {
 
 // Leer datos
 
-// Realizar calculo
+/**
+ * Multiplica dos matrices bidimensionales.
+ * Las dimensiones de las matrices a multiplicar y el producto deben ser validas:
+ *    matA[n][m] * matB[m][o] = matC[n][o]
+ */
+void matrix_multiplication(
+    MatDims *dimsA, double matA[dimsA->y][dimsA->x],
+    MatDims *dimsB, double matB[dimsB->y][dimsB->x],
+    MatDims *dimsC, double matC[dimsC->y][dimsC->x]
+) {
+    int i, j, k;
+    for (i=0; i<dimsC->y; i++) {
+        for (j=0; j<dimsC->x; j++) {
+            matC[i][j] = 0.0;
+            for (k=0; k<dimsA->x; k++) {
+                matC[i][j] += matA[i][k] * matB[k][j];
+            }
+        }
+    }
+}
 
 // Escribir resultados
 
 
 int main() {
-    // Datos de prueba
-    MatDims dims = {2, 3};
-    double matriz[2][3] = { {1, 2, 3},
-                            {4, 5, 6} };
+    // Datos de prueba (borrar cuando se puedan leer las matrices de un archivo)
+    MatDims dimsA = {2, 3}, dimsB = {3, 1}, dimsC = {2, 1};
+    double matA[2][3] = { {1, 2, 3},
+                          {4, 5, 6} };
+    double matB[3][1] = { {1},
+                          {2},
+                          {3} };
+    double matC[2][1];
 
     // Llamada a leer datos
-    // Llamada a realizar calculo
+
+    // Validar dimensiones de matrices
+    if (dimsA.x != dimsB.y || dimsA.y != dimsC.y || dimsB.x != dimsC.x) {
+        printf("Dimensiones de matrices invalidas: A[%d][%d] x B[%d][%d] = C[%d][%d]\n", dimsA.y, dimsA.x, dimsB.y, dimsB.x, dimsC.y, dimsC.x);
+        return 1;
+    }
+
+    // Obtener el producto de las matrices A y B
+    matrix_multiplication(&dimsA, matA, &dimsB, matB, &dimsC, matC);
+
     // Llamada a escribir resultados
 
-    // Imprimir la matriz
-    print_matrix(&dims, matriz);
+    // Imprimir la matriz producto (borrar cuando se puedan imprimir matrices en un archivo)
+    print_matrix(&dimsC, matC);
 
     return 0;
 }
